a5e0797fa4938c9d18872a4fa12e39a8e2861d0d
matheusDev <matheus.info29@gmail.com>
2023-09-05 08:05:09 -0300

    Exercício 01 -  CONDIÇÕES

---
+++ curso_JavaScript/exer003-operadores/exer01/exer01.js
@@ -0,0 +1,7 @@
+var vel = 12;
+console.log(`A velocidade do seu carro é ${vel}Km/h`);
+if (vel > 60) { //condição simples
+    console.log('Você ultrapaçou a velocidade permitida, MULTADO!!!');
+}
+console.log('Dirija com cuidade!!! Use sempre o cinto de segurança, mesmo em viajens curtas');
+
