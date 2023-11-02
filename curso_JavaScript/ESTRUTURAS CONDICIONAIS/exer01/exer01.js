a0d1f849232590d26471db65d5293170afafc9db
matheusDev <matheus.info29@gmail.com>
2023-11-02 18:27:41 -0300

    Update

---
+++ curso_JavaScript/ESTRUTURAS CONDICIONAIS/exer01/exer01.js
@@ -0,0 +1,9 @@
+console.log("hello world");
+
+var vel = 150;
+
+if(vel>60){
+    console.log(`Você ultrapaçou a velocidade permitida!!! MULTADO!!!`)
+}else{
+    console.log("Use sempre o cinto de segurança.")
+}
