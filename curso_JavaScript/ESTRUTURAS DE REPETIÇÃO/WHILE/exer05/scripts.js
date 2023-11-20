128aeb15d273f3888a0d8dfe97157a25ed85559c
matheusDev <matheus.info29@gmail.com>
2023-11-20 13:42:14 -0300

    update

--- curso_JavaScript/ESTRUTURAS DE REPETIÇÃO/WHILE/exer05/scripts.js
+++ curso_JavaScript/ESTRUTURAS DE REPETIÇÃO/WHILE/exer05/scripts.js
@@ -3,3 +3,17 @@ var result = window.document.getElementById("result");
 var aleatorio = Math.floor(Math.random()*10);
 var verificar = window.document.getElementById("verificar");
 
+verificar.addEventListener("click", verificarAleatorio)
+
+function verificarAleatorio() {
+
+    while (txt1 != aleatorio) {
+     result.innerHTML=aleatorio;   
+    }
+
+    result.innerHTML=`O número digitado é igual ao aleatório ${aleatorio}`
+
+}
+
+
+
