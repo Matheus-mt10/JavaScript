efd2e95d350fd329363e20b1e9515bba56efc78c
matheusDev <matheus.info29@gmail.com>
2023-11-20 10:05:20 -0300

    update

--- curso_JavaScript/ESTRUTURAS DE REPETIÇÃO/WHILE/exer05/scripts.js
+++ curso_JavaScript/ESTRUTURAS DE REPETIÇÃO/WHILE/exer05/scripts.js
@@ -1,8 +1,5 @@
 var txt1 = window.document.getElementById("txt1");
 var result = window.document.getElementById("result");
 var aleatorio = Math.floor(Math.random()*10);
-
-while(txt1 != aleatorio ){
-    result.innerHTML=`${txt1}`
-}
+var verificar = window.document.getElementById("verificar");
 
