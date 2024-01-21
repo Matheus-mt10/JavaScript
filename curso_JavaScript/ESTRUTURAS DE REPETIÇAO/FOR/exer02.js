10139bd93fffeb77602b8550272568d38b035426
matheusDev <matheus.info29@gmail.com>
2024-01-21 12:30:12 -0300

    Update exercicíos

--- curso_JavaScript/ESTRUTURAS DE REPETIÇAO/FOR/exer02.js
+++ curso_JavaScript/ESTRUTURAS DE REPETIÇAO/FOR/exer02.js
@@ -1,7 +1,12 @@
 console.log("TABUADA DO 5")
-for(let i=0; i<51;i+5){
+let j = 0
+for(let i=0; i<=50;i+=5){
+
     const trava5 = 5;
-    let j = 0;
     j++
+    
+
+  
     console.log(`${trava5}X${j} = ${i}`)
+    
 }
--- curso_JavaScript/ESTRUTURAS DE REPETIÇAO/FOR/node_f636f258fccbc.tmp.js
+++
@@ -1,7 +0,0 @@
-console.log("TABUADA DO 5")
-for(let i=0; i<51;i+5){
-    const trava5 = 5;
-    let j = 0;
-    j++
-    console.log(`${trava5}X${j} = ${i}`)
-}
