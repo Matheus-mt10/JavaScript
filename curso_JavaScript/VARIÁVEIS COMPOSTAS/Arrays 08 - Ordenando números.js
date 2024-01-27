3dc05f086f73ae3e95e31e27fcccc0953c31f552
matheusDev <matheus.info29@gmail.com>
2024-01-27 07:55:26 -0300

    Update

---
+++ curso_JavaScript/VARIÁVEIS COMPOSTAS/Arrays 08 - Ordenando números.js
@@ -0,0 +1,8 @@
+/* Crie um array com números e ordene-os em ordem crescente. Exiba o array ordenado no console.*/
+
+let numeros = [1,2,3,4,5,50,66,78,99,12,24]
+numeros.sort()
+for(let i=0;i<numeros.length;i++){
+    
+    console.log(numeros);
+}
