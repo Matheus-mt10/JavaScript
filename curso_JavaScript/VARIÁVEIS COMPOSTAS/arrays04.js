8d5a2e7f885b66b5854dc2dd81a5f06a288bd2b5
matheusDev <matheus.info29@gmail.com>
2024-01-26 21:31:26 -0300

    Update exercício média de idades

--- curso_JavaScript/VARIÁVEIS COMPOSTAS/arrays04.js
+++ curso_JavaScript/VARIÁVEIS COMPOSTAS/arrays04.js
@@ -1,10 +1,16 @@
-let nomes = ["Matheus", "Camila", "Fátima"]
-let idades = [24,22,48]
-
+let nomes= ["Matheus ", "Camila ", "Fátima "]
+let idades = [24,22,53]
+let soma=0
+var usaNome = ""
 
 for(let cont=0; cont<idades.length;cont++){
-    let mediaIdade = idades[cont]
-    let result = mediaIdade/3
-    console.log(result)
+   soma+=idades[cont]
+   var media = soma/3
 }
 
+for(let i=0;i<nomes.length;i++){
+    usaNome+= nomes[i]
+}
+
+console.log(usaNome)
+console.log(`A média de idade de ${usaNome} é: ${media}`)
