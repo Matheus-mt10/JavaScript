f18f22e0e7a5e84f1e84d989b8e2d4dda9c95be9
matheusDev <matheus.info29@gmail.com>
2024-02-09 00:31:42 -0300

    Update

---
+++ curso_JavaScript/CLASSES E OBJETOS/AULA01 - Classe.js
@@ -0,0 +1,9 @@
+class formaDeBolo{
+    construtor(saborMassa,saborRecheio){
+        this.saborMassa = saborMassa
+        this.saborRecheio = saborRecheio
+    }
+}
+
+let boloDeFesta = new formaDeBolo("Massa de chocolate","recheio de nutella")
+console.log(boloDeFesta)
---
+++ curso_JavaScript/CLASSES E OBJETOS/teste.js
@@ -0,0 +1,11 @@
+class pcBuild{
+    constructor(tipoPc,processador,ram){
+        this.tipoPc = tipoPc
+        this.processador = processador
+        this.ram = ram
+    }
+        
+}
+
+let pc1 = new pcBuild ("Notebook", "R5 5450U", "8GB")
+console.log(pc1);
